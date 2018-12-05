752609d8b0cc1ca62c4bfff13c57815dd02d5b48
chezi008 <chezi008@qq.com>
2018-12-05 17:41:33 +0800

    NormalTabLayout控件添加textColor属性

--- baseui/src/main/java/com/chezi008/baseui/view/NormalTabLayout.kt
+++ baseui/src/main/java/com/chezi008/baseui/view/NormalTabLayout.kt
@@ -28,9 +28,16 @@ open class NormalTabLayout : RelativeLayout {
     constructor(ctx: Context) : this(ctx, null)
 
     constructor(ctx: Context, attr: AttributeSet?) : super(ctx, attr) {
+        initAttrs(ctx,attr)
         initView(mTabLayout)
     }
 
+    private var textColor:Int=0
+    private fun initAttrs(context: Context, attrs: AttributeSet?) {
+        val ta = context.obtainStyledAttributes(attrs, R.styleable.NormalTabLayout)
+        textColor = ta.getResourceId(R.styleable.NormalTabLayout_textColor,  R.color.default_color_tab_text)
+        ta.recycle()
+    }
 
 
     open fun initView(tabLayout: TabLayout) {
@@ -86,6 +93,8 @@ open class NormalTabLayout : RelativeLayout {
             textView.textSize = 12.0f
             textView.setPadding(5, 5, 5, 10)
             textView.gravity = Gravity.CENTER
+            var colors = resources.getColorStateList(textColor)
+            textView.setTextColor(resources.getColorStateList(textColor))
             view.addView(textView)
         }
         return view
